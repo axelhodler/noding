f73ceebdf59323fe6a8afb74129fdfe7faa8ee00
xorrr <derxor@gmail.com>
2014-04-12 01:46:45 +0200

    add test to show how to test the content of the response body

--- tests/test_server.js
+++ tests/test_server.js
@@ -6,10 +6,22 @@ var assert = require('assert');
 
 describe('server functionality', function() {
   describe('server works', function() {
+    server.listen();
     it('should respond with status code 200', function() {
-      server.listen();
       http.get('http://127.0.0.1:1337', function(res) {
         assert.equal(200, res.statusCode);
+      });
+    });
+
+    it('should return the static index.html file', function() {
+      http.get('http://127.0.0.1:1337', function(res) {
+        var body = '';
+        res.on('data', function(chunk) {
+          body += chunk;
+        });
+        res.on('end', function() {
+          assert.equal('hello', body);
+        });
       });
     });
   });
